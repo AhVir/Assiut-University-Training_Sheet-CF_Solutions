cfb718983e346508e5f2829a526e19f2ed781ddd
Sheikh Tanvir Ahmed <110638482+AhVir@users.noreply.github.com>
2024-09-06 06:35:11 +0600

    Add files via upload

---
+++ Sheet 02/B_Even_Numbers.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define debugIn freopen("out.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define br cout << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int n; cin >> n;
+    if(n == 1){
+        cout << -1 << endl;
+        return;
+    }
+    for(int i=1; i<=n; i++){
+        if(i%2 == 0) cout << i << endl;
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
